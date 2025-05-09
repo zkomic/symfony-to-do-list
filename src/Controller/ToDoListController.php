66ce409e16d9a89f1daff6a30edeb7dc66f5dbcc
Zrinka <60172572+zkomic@users.noreply.github.com>
2025-05-09 10:14:45 +0200

    crud methods
    php bin/console make:entity
    php bin/console make:migration
    php bin/console doctrine:migrations:migrate

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,27 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
 {
+    private ManagerRegistry $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
     /**
      * @Route("/", name="to_do_list")
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $tasks = $this->managerRegistry->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+
+        return $this->render('index.html.twig', ['tasks' => $tasks]);
     }
 
     /**
      * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function createTask(): Response
+    public function createTask(Request $request): Response
     {
-        exit('TODO: Create a new task!');
+        $title = trim($request->request->get('title'));
+        if (empty($title)) {
+            return $this->redirectToRoute('to_do_list');
+        }
+        $entityManager = $this->managerRegistry->getManager();
+        $task = new Task;
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
     }
 
     /**
@@ -30,7 +52,12 @@ class ToDoListController extends AbstractController
      */
     public function changeTaskStatus(int $id): Response
     {
-        exit(sprintf('TODO: Change status of task with ID: %d!', $id));
+        $entityManager = $this->managerRegistry->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
     }
 
     /**
@@ -38,6 +65,11 @@ class ToDoListController extends AbstractController
      */
     public function deleteTask(int $id): Response
     {
-        exit(sprintf('TODO: Delete task with ID: %d!', $id));
+        $entityManager = $this->managerRegistry->getManager();
+        $task = $this->managerRegistry->getRepository(Task::class)->find($id);
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
     }
 }
